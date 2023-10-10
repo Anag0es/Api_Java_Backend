90835351fb8b0b1f2809b6dd2e5856406f0c5913
AnaBarb0sa <ana.barbosa@sou.inteli.edu.br>
2023-10-10 19:10:04 -0300

    test: conhecendo o método `Not Blank`

--- User-api/src/main/java/com/api/backend/Userapi/dto/UserDTO.java
+++ User-api/src/main/java/com/api/backend/Userapi/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.api.backend.Userapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+
+    @NotBlank(message = "nome é obrigatório")
     private String nome;
+    @NotBlank(message = "cpf é obrigatório")
     private String cpf;
+    @NotBlank(message = "endereco é obrigatório")
     private String endereco;
+    @NotBlank(message = "e-mail é obrigatório")
     private String email;
     private String telefone;
     private LocalDateTime dataCadastro;
