6739ecf07cf3959221f2c849da493a0e4b67f6b3
Anag0es <analuisa.goes2004@gmail.com>
2023-10-11 14:55:12 -0300

    feat: adicao de entidades category e product

---
+++ product-api/src/main/java/com/api/backend/productapi/model/Category.java
@@ -0,0 +1,30 @@
+package com.api.backend.productapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+
+    public static Category convert(CategoryDTO categoryDTO){
+        Category category = new Category();
+        category.setNome(categoryDTO.getNome());
+        category.setId(categoryDTO.getId());
+        return category;
+    }
+}
