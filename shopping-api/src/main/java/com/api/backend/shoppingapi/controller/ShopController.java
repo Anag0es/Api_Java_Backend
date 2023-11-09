fcb6b10dd09f1086554f981f5eb193d63db9f1f6
Eduardo Santana <ezambomsantana@gmail.com>
2023-11-09 08:22:47 -0300

    fix small errors

--- shopping-api/src/main/java/com/api/backend/shoppingapi/controller/ShopController.java
+++ shopping-api/src/main/java/com/api/backend/shoppingapi/controller/ShopController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,7 +36,7 @@ public class ShopController {
         return service.findById(id);
     }
 
-    @GetMapping("/shopping")
+    @PostMapping("/shopping")
     @ResponseStatus(HttpStatus.CREATED)
     public ShopDTO newShop(@Valid @RequestBody ShopDTO shopDTO){
         return service.save(shopDTO);
