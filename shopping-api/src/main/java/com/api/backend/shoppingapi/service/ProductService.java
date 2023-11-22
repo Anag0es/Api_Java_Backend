4ba6d9f20b90dccb967052a52b9111bf5cbf3dff
Anag0es <analuisa.goes2004@gmail.com>
2023-11-21 22:02:00 -0300

    feat: (shopping-api) adicionando comunicacao com product-api

--- shopping-api/src/main/java/com/api/backend/shoppingapi/service/ProductService.java
+++ shopping-api/src/main/java/com/api/backend/shoppingapi/service/ProductService.java
@@ -1,8 +1,28 @@
 package com.api.backend.shoppingapi.service;
 
+import com.api.backend.productapi.dto.ProductDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ProductService {
+    private String productApiUrl = "http://localhost:8081";
 
+    public ProductDTO getProductByIdentifier(String productIdentifier){
+        try{
+            WebClient webClient = WebClient.builder()
+                    .baseUrl(productApiUrl)
+                    .build();
+
+            Mono<ProductDTO> product = webClient.get()
+                    .uri("/product/" + productIdentifier)
+                    .retrieve()
+                    .bodyToMono(ProductDTO.class);
+
+            return product.block();
+        }catch (Exception e){
+            throw new RuntimeException("Product not found.");
+        }
+    }
 }
